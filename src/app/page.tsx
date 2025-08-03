f4d645e95832fae47e7b4a7982b90dcdacfc1bf6
Saikat Sardar <saikatsardar5@gmail.com>
2025-08-03 17:49:46 +0530

    Add call-to-action section with background image
    Introduces a new call-to-action section to the homepage, featuring a background image and a prompt to purchase a postcard. Adds the corresponding image file to the public directory.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,9 +16,11 @@ const page = () => {
 					<h1 className="text-8xl tracking-widest font-semibold text-earth-dark">
 						EARTH
 					</h1>
+
 					<p className="text-4xl uppercase tracking-widest font-light">
 						Multipurpose Store
 					</p>
+
 					<Link
 						href={""}
 						className="text-xl uppercase px-8 py-2 bg-earth-light hover:bg-earth-dark text-white rounded-sm mt-4">
@@ -148,6 +150,22 @@ const page = () => {
 						</div>
 					</div>
 				</div>
+			</section>
+
+			<section className="bg-[url(/call-to-action.jpg)] bg-center bg-cover py-28 px-48 grid place-items-center gap-8">
+				<div className="text-5xl font-semibold uppercase">
+					Give the Gift of a Postcard
+				</div>
+
+				<div className="text-lg">
+					Give the gift of a lasting memory with a postcard
+				</div>
+
+				<Link
+					href={""}
+					className="text-xl uppercase px-8 py-2 bg-earth-light hover:bg-earth-dark text-white rounded-sm mt-4">
+					Purchase A Postcard
+				</Link>
 			</section>
 		</>
 	);

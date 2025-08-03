7ccc29c124fe68580efd8f754d1fd3da31232029
Saikat Sardar <saikatsardar5@gmail.com>
2025-08-03 17:16:40 +0530

    Add poster images and display section to homepage
    Added three new poster images to the public directory and updated the homepage to display a new section featuring these posters with images, titles, and prices using the Next.js Image component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import Image from "next/image";
 import Link from "next/link";
 
 export const metadata: Metadata = {
@@ -24,6 +25,52 @@ const page = () => {
 					</Link>
 				</div>
 			</section>
+
+			<section className="py-28 px-48 grid grid-cols-3 gap-6 border-b border-gray-300">
+				<div className="grid gap-1">
+					<Image
+						src={"/poster1.jpg"}
+						alt="poster1.jpg"
+						width={350}
+						height={350}
+						className="aspect-square h-auto w-auto rounded-sm"
+					/>
+
+					<div className="text-gray-500">Posters</div>
+					<div className="font-bold text-lg uppercase">Poster V1</div>
+					<div className="font-semibold">₹239.00</div>
+				</div>
+
+				<div className="grid gap-1">
+					<Image
+						src={"/poster2.jpg"}
+						alt="poster2.jpg"
+						width={350}
+						height={350}
+						className="aspect-square h-auto w-auto rounded-sm"
+					/>
+
+					<div className="text-gray-500">Posters</div>
+					<div className="font-bold text-lg uppercase">Poster V2</div>
+					<div className="font-semibold">₹212.00</div>
+				</div>
+
+				<div className="grid gap-1">
+					<Image
+						src={"/poster3.jpg"}
+						alt="poster3.jpg"
+						width={350}
+						height={350}
+						className="aspect-square h-auto w-auto rounded-sm"
+					/>
+
+					<div className="text-gray-500">Posters</div>
+					<div className="font-bold text-lg uppercase">Poster V3</div>
+					<div className="font-semibold">₹251.00</div>
+				</div>
+			</section>
+
+			<section className="py-28 px-48"></section>
 		</>
 	);
 };

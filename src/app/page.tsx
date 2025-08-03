c78d8315a11afb54b870891af29170a57d0533cd
Saikat Sardar <saikatsardar5@gmail.com>
2025-08-03 14:07:50 +0530

    Add home banner and update homepage layout
    Added a new home-banner.jpg image and updated the homepage to include a hero section with background image, title, subtitle, and a 'Shop Now' button. Also introduced custom earth color variables in globals.css and imported SiteHeader component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import SiteHeader from "@/components/SiteHeader";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
 	title: "Home | Planet Earth Store",
@@ -8,7 +10,23 @@ export const metadata: Metadata = {
 const page = () => {
 	return (
 		<>
-			<div className="">Home Page</div>
+			<SiteHeader />
+
+			<section className="bg-[url(/home-banner.jpg)] bg-center bg-cover py-64 grid place-items-center">
+				<div className="grid place-items-center gap-6">
+					<h1 className="text-8xl tracking-widest font-semibold text-earth-dark">
+						EARTH
+					</h1>
+					<p className="text-4xl uppercase tracking-widest font-light">
+						Multipurpose Store
+					</p>
+					<Link
+						href={""}
+						className="text-xl uppercase px-8 py-2 bg-earth-light hover:bg-earth-dark text-white rounded-sm mt-4">
+						Shop Now
+					</Link>
+				</div>
+			</section>
 		</>
 	);
 };

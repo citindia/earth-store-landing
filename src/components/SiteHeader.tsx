44ef64c38165a77c8d126be1dbc153bd70be211b
Saikat Sardar <saikatsardar5@gmail.com>
2025-08-03 14:06:24 +0530

    Add SiteHeader component and logo image
    Introduces a new SiteHeader React component with navigation links and a logo image. The logo asset is added to the public directory for use in the header.

---
+++ src/components/SiteHeader.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const SiteHeader = () => {
+	return (
+		<header className="flex justify-between items-center py-8 px-36 absolute left-0 top-0 right-0">
+			<Link href="/">
+				<Image
+					src="/logo.png"
+					alt="logo"
+					width={200}
+					height={20}
+				/>
+			</Link>
+
+			<nav className="uppercase tracking-widest text-lg flex gap-8">
+				<Link href="/">Home</Link>
+				<Link href="/about">About</Link>
+				<Link href="/shop">Shop</Link>
+				<Link href="/contact">Contact</Link>
+			</nav>
+		</header>
+	);
+};
+
+export default SiteHeader;
